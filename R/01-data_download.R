ed82f0bdcc1e68a249e4f7bc7fbe4d32bfdcaafb
fabienne-moana <fkappeler@ethz.ch>
2024-06-03 16:02:17 +0000

    changed variable type character to date

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -3,3 +3,8 @@ library(googlesheets4)
 raw_data <- read_sheet("https://docs.google.com/spreadsheets/d/12rU6WmPDLHlAwD8pHjUrLh5PyxqVdM6BKoPx2CaZR1E/edit?usp=sharing")
 
 write.csv(raw_data, "data/raw/raw_data.csv")
+
+
+dictionary <- read_sheet("https://docs.google.com/spreadsheets/d/1rmat_gGZk4y1CRBI8qhRuayoYUJrJeuzLZvhQ-L2PwI/edit?usp=sharing")
+
+write.csv(dictionary, "data/processed/dictionary.csv")
--- R/02-data_cleaning.R
+++ R/02-data_cleaning.R
@@ -7,10 +7,9 @@ data_processed <- read.csv("data/raw/raw_data.csv")
 glimpse(data_processed)
 
 data_processed <- data_processed |> 
-  mutate(date = as.Date(date, origin = "1899-12-30"))
-
-#change time
+  mutate(date_time = as.POSIXct(date_time, "%Y:%M:%D %H:%M",))
 
 glimpse(data_processed)
 
 write.csv(data_processed, "data/processed/data_processed.csv")
+
